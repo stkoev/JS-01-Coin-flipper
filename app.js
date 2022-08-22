5dfa76c41bcf7b7f627279102bc23b4b17bf7375
Stefan Koev <st.koev@outlook.com>
2022-08-22 10:05:18 +0300

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const body = document.getElementsByTagName("body")[0];
+
+const getRndNum = function (min, max) {
+  const rnd = Math.random() * (max - min) + min;
+  return Math.floor(rnd);
+};
+
+const getRndColor = function (arr, num) {
+  return arr[num];
+};
+
+btn.addEventListener("click", function () {
+  // Get random number between 0 and colors.length
+  const randomNumber = getRndNum(0, colors.length);
+  const randomColor = getRndColor(colors, randomNumber);
+  console.log(randomNumber, randomColor);
+
+  // change background color to:
+  body.style.backgroundColor = randomColor;
+
+  // show new color in span
+  color.textContent = randomColor;
+});
--- hex.js
+++ hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const getRndNum = function (min, max) {
+  const rnd = Math.random() * (max - min) + min;
+  return Math.floor(rnd);
+};
+
+const getRndColor = function () {
+  let hexColor = "#";
+  for (let index = 0; index < 6; index++) {
+    hexColor += hex[getRndNum(0, 16)];
+  }
+  return hexColor;
+};
+
+btn.addEventListener("click", function () {
+  const hexColor = getRndColor();
+  console.log(hexColor);
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
